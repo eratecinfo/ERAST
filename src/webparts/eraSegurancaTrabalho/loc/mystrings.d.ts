af5799e02220165248c4cbb9c0a587d9e3520a72
eduardojoseamaral <eduardo.amaral@eratecinfo.com.br>
2018-02-22 16:31:51 -0300

    commit

---
+++ src/webparts/eraSegurancaTrabalho/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IEraSegurancaTrabalhoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'EraSegurancaTrabalhoWebPartStrings' {
+  const strings: IEraSegurancaTrabalhoWebPartStrings;
+  export = strings;
+}
